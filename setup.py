895e9179ceb26e19153121d189596e8241a8d6b4
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2020-09-09 00:09:01 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,19 @@ from setuptools import setup, find_packages
 
 setup(
     name="torchph",
-    version="0.0.0",
-    packages=setuptools.find_packages(exclude=('tests*',))
+    version="0.1.0",
+    author="Sayantan Das",
+    author_email="sayantandas30011998@gmail.com",
+    description="torchph",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/forkbabu/torchph",
+    packages=setuptools.find_packages(exclude=('tests*',)),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    keywords='Persistent Homology',
 )
